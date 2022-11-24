18cfbd76a0155264f2b971fc58b60d4c8f30caa5
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-11-24 09:54:04 +0300

    added an editing mode to Today and written code that applies different snapshots for the view and editing modes.

---
+++ MyToday/DetailViewController/ReminderViewController+Section.swift
@@ -0,0 +1,30 @@
+//
+//  ReminderViewController+Section.swift
+//  MyToday
+//
+//  Created by Дмитрий Дуров on 24.11.2022.
+//
+
+import Foundation
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+        
+        var name: String {
+            switch self {
+            case .view:
+                return ""
+            case .title:
+                return NSLocalizedString("Title", comment: "Title section name")
+            case .date:
+                return NSLocalizedString("Date", comment: "Date section name")
+            case .notes:
+                return NSLocalizedString("Notes", comment: "Notes section name")
+            }
+        }
+    }
+}
