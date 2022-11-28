aab5cc2044087e699c7c30e1c15bd08863080bdd
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-11-28 11:37:56 +0300

    added a valuable editing feature to your app.

---
+++ MyToday/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  MyToday
+//
+//  Created by Дмитрий Дуров on 28.11.2022.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
